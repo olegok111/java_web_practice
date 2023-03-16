2457e0103e11b601310a335357b1d6fb7a91f8ec
Oleg Okoyomov <olegokoyomov2@gmail.com>
2023-03-16 14:07:06 +0300

    HibernateDatabaseConfig BUGS: - app properties aren't getting recognized

---
+++ src/main/java/msu/cmc/jaweb/config/HibernateDatabaseConfig.java
@@ -0,0 +1,50 @@
+package msu.cmc.jaweb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class HibernateDatabaseConfig {
+    @Value("org.postgresql.Driver")
+    private String DB_DRIVER;
+    @Value("jdbc:postgresql://localhost/postgres")
+    private String DB_URL;
+    @Value("postgres")
+    private String DB_USERNAME;
+    @Value("0000")
+    private String DB_PASSWORD;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(oraDataSource());
+        sessionFactory.setPackagesToScan("ru.msu.cmc.webprak.models");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        hibernateProperties.setProperty("connection_pool_size", "1");
+
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource oraDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(DB_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+
+        return dataSource;
+    }
+}
