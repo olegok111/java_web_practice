1d784aeeee309e955a1314b91215c4f4f0490bf2
Oleg Okoyomov <olegokoyomov2@gmail.com>
2023-05-07 19:05:20 +0300

    Added htmls and WebConfig

--- src/main/java/msu/cmc/jaweb/controllers/ErrorPage.java
+++ src/main/java/msu/cmc/jaweb/controllers/ErrorPage.java
@@ -4,15 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ErrorController {
+public class ErrorPage {
 
     @GetMapping(value = "/error")
     public String error() {
         return "error";
     }
-    
-    @GetMapping(value = "/")
-    public String index() {
-        return "index";
-    }
+
 }
---
+++ src/main/java/msu/cmc/jaweb/controllers/MainPage.java
@@ -0,0 +1,14 @@
+package msu.cmc.jaweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPage {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
