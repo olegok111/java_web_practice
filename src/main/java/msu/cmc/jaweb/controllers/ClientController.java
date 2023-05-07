4f2fa2e29193828f392f8d4c6f2c35c917f27e85
Oleg Okoyomov <olegokoyomov2@gmail.com>
2023-05-07 23:08:38 +0300

    First ver of ClientController

--- src/main/java/msu/cmc/jaweb/controllers/ClientController.java
+++ src/main/java/msu/cmc/jaweb/controllers/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import msu.cmc.jaweb.dao.ClientDao;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,6 +15,11 @@ public class ClientController {
 
     @Autowired
     private final ClientDao clientDao = new ClientDaoImpl();
+
+    @GetMapping("/clients")
+    public String clientsPage(Model model) {
+        return "clients";
+    }
 
     @GetMapping("/clientInfo")
     public String clientInfoPage(@RequestParam(name = "clientId") Long clientId, Model model) {
@@ -28,4 +34,35 @@ public class ClientController {
         return "clientInfo";
     }
 
+    @GetMapping("/clientEdit")
+    public String clientEditPage(@RequestParam(name = "clientId") Long clientId, Model model) {
+
+        return "clientEdit";
+    }
+
+    @PostMapping("/clientSave")
+    public String clientSave(@RequestParam(name = "clientId") Long clientId,
+                             @RequestParam(name = "fullName") String fullName,
+                             @RequestParam(name = "email") String email,
+                             @RequestParam(name = "phone") String phone,
+                             Model model) {
+        Client client = clientDao.getById(clientId);
+
+        if (client == null) {
+            client = new Client(clientId, fullName, email, phone);
+        } else {
+            client.setFull_name(fullName);
+            client.setEmail(email);
+            client.setPhone(phone);
+        }
+
+        model.addAttribute("error_msg", "Данные не сохранены");
+        return "error";
+    }
+
+    @PostMapping("/clientRemove")
+    public String clientRemove(@RequestParam(name = "clientId") Long clientId) {
+        clientDao.deleteById(clientId);
+        return "redirect:/clients";
+    }
 }
